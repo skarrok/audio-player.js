27020c1bc06819177d291fed7f2520817addfa45
Ivan Petukhov <satels@gmail.com>
2020-08-21 18:03:44 +0300

    fix

--- dist/js/audio-player.js
+++ dist/js/audio-player.js
@@ -141,6 +141,7 @@ function CtPlayer(el) {
             } else {
                 this.wavesurfer.empty()
                 this.playPause.attributes.d.value = "M18 12L0 24V0"
+                this.playpauseBtn.style.display = 'none'
                 this.wavesurfer.setCurrentTime(0)
                 this.currentTime.textContent = this.formatTime(0)
                 this.totalTime.textContent = this.formatTime(0)
